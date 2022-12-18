0893665736b0e7010274619d986992f1e9de0e10
Your Name <you@example.com>
2022-12-18 22:20:14 +0100

    Added script and style

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="bg-dgBlue rounded-base w-112 flex content-center flex-col flex-wrap px-8 pt-12 box-content mobile:px-5 mobile:w-80">
+    <label class="font-manrope text-nGreen tracking-label text-center text-base mb-6">ADVICE #{{advice.id}}</label>
+    <h2 class="font-manrope font-extrabold text-quote text-lCyan text-center mb-8">"{{advice.advice}}"</h2>
+    <DividerDesktop class="mx-auto mobile:hidden" />
+    <DividerMobile class="mx-auto desktop:hidden tablet:hidden" />
+    <button @click="getAdvice" class="transition ease-in-out delay-150 bg-nGreen w-14 h-14 flex content-center justify-center flex-wrap rounded-full mx-auto relative top-bottom hover:drop-shadow-button"><IconDice /></button>
+  </div>
+  <!-- <div class="font-manrope">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+    Coded by <a href="#">Your Name Here</a>.
+  </div> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import DividerDesktop from './components/icons/DividerDesktop.vue'
+import DividerMobile from './components/icons/DividerMobile.vue'
+import IconDice from './components/icons/IconDice.vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios';
+
+const advice = ref({})
+
+onMounted(() => getAdvice())
+
+async function getAdvice() {
+  axios.get("https://api.adviceslip.com/advice").then(res => {
+    advice.value = res.data.slip
+    console.log(res.data.slip)
+  })
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
